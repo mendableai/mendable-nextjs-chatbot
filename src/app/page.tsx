1e9b40675b6706145e4a2d91eb708eabe3621b54
Nicolas <nicolascamara29@gmail.com>
2023-07-12 19:33:54 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,9 +17,9 @@ export default function Home() {
         <PopoverTrigger asChild className="fixed bottom-4 right-4">
           <Button
             variant="outline"
-            className="rounded-full w-16 h-16 aspect-square"
+            className="rounded-full w-16 h-16 aspect-square bg-black hover:bg-gray-700"
           >
-            <MessageCircle size={40} />
+            <MessageCircle size={40} className="text-white" />
           </Button>
         </PopoverTrigger>
       </Popover>
